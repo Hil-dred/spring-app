a0cd5e9ba397baaf1e76fe4fffd23aa0293780f5
Ubuntu <ubuntu@ip-172-31-32-150.us-east-2.compute.internal>
2022-03-03 18:55:16 +0000

    CD pipeline test

---
+++ gitactions_pipeline_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+git pull origin master
+mvn package
+#docker kill hildredadebayo/spring-demo:latest
+#docker rmi hildredadebayo/spring-demo:latest -f
+docker build -t hildredadebayo/spring-demo:latest .
+docker login
+docker push hildredadebayo/spring-demo:latest
+docker run -d --rm -p 7030:8080 hildredadebayo/spring-demo:latest 
+sudo systemctl restart nginx
+
