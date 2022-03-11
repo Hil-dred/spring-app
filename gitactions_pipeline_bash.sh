61ddb79c654fd574010186463200367507f89ed2
Ubuntu <ubuntu@ip-172-31-32-150.us-east-2.compute.internal>
2022-03-11 22:46:42 +0000

    Modify

--- gitactions_pipeline_bash.sh
+++ gitactions_pipeline_bash.sh
@@ -5,9 +5,9 @@ git pull origin master
 mvn package
 #docker kill hildredadebayo/spring-demo:latest
 #docker rmi hildredadebayo/spring-demo:latest -f
-sudo docker build -t hildredadebayo/spring-demo:latest .
+docker build -t hildredadebayo/spring-demo:latest .
 #docker login
 #docker push hildredadebayo/spring-demo:latest
-sudo docker run -d --rm -p 7030:8080 hildredadebayo/spring-demo:latest 
+docker run -d --rm -p 7030:8080 hildredadebayo/spring-demo:latest 
 sudo systemctl restart nginx
 
