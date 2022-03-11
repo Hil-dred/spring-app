6e9bc2a69a60f73fbcda4ff9ab5fa2b4966e948d
Ubuntu <ubuntu@ip-172-31-32-150.us-east-2.compute.internal>
2022-03-11 21:57:44 +0000

    Change mode

--- gitactions_pipeline_bash.sh
+++ gitactions_pipeline_bash.sh
@@ -6,8 +6,8 @@ mvn package
 #docker kill hildredadebayo/spring-demo:latest
 #docker rmi hildredadebayo/spring-demo:latest -f
 docker build -t hildredadebayo/spring-demo:latest .
-docker login
-docker push hildredadebayo/spring-demo:latest
+#docker login
+#docker push hildredadebayo/spring-demo:latest
 docker run -d --rm -p 7030:8080 hildredadebayo/spring-demo:latest 
 sudo systemctl restart nginx
 
