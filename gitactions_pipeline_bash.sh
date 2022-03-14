0bd502de3c1618585781a8367e91b1a545f34823
Ubuntu <ubuntu@ip-172-31-32-150.us-east-2.compute.internal>
2022-03-14 16:43:33 +0000

    Commit

--- gitactions_pipeline_bash.sh
+++ gitactions_pipeline_bash.sh
@@ -2,9 +2,10 @@
 ## Gitactions Pipeline
 
 git pull origin main
-mvn package
-docker kill hildredadebayo/spring-demo:latest
+#mvn package
+#docker kill hildredadebayo/spring-demo:latest
 docker rmi hildredadebayo/spring-demo:latest -f
+mvn package
 docker build -t hildredadebayo/spring-demo:latest .
 #docker login
 #docker push hildredadebayo/spring-demo:latest
