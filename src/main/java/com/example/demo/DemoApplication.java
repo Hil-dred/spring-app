b8d032aac666d77a59ea5a7aab0aaed7a96227de
Ubuntu <ubuntu@ip-172-31-32-150.us-east-2.compute.internal>
2022-03-16 16:05:44 +0000

    Fifth Mod

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,7 +20,7 @@ public class DemoApplication {
 
 	@GetMapping("/tobi/")
 	public String profile1(){
-		return "I am a Devops Engineer. P.S: Jenkins is annoying!";
+		return "I am a Devops Engineer. P.S: Jenkins is annoying!....On to Kubernetes!!";
 	}
 
 	@GetMapping("/wale/")
