e2ea64e906727b40cd4943f88ebc4480426c7cd6
Hildred Adebayo <68189229+Hil-dred@users.noreply.github.com>
2022-09-12 20:28:31 +0100

    Update gitactions_pipeline_bash.sh

--- gitactions_pipeline_bash.sh
+++ gitactions_pipeline_bash.sh
@@ -7,8 +7,7 @@ mvn package
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=hildredadebayo/spring-demo:latest --format="{{.ID}}"))
 docker rmi hildredadebayo/spring-demo:latest -f
 docker build -t hildredadebayo/spring-demo:latest .
-#docker login
-#docker push hildredadebayo/spring-demo:latest
+docker login
+docker push hildredadebayo/spring-demo:latest
 docker run -d --rm -p 7070:8080 hildredadebayo/spring-demo:latest 
 sudo systemctl restart nginx
-
